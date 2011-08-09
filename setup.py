5916960c974ce72c74025c43b0e46bdac8518fee
Mark Holmquist <marktraceur@gmail.com>
2011-08-08 18:02:38 -0700

    + install script (try it, it's easy!) + closed 0.13 TODO item

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='stattr',
+      version='0.12',
+      description='A web application for tracking competitive activities',
+      author='Mark Holmquist',
+      author_email='marktraceur@gmail.com',
+      url='https://github.com/MarkTraceur/stattr',
+      packages=['statserv'],
+      package_dir={'statserv': 'statserv'},
+      package_data={'statserv': ['stattrd.conf', 'tpls/*.tpl', 'static/*.html',
+                                 'static/*.js', 'static/*.css']},
+      scripts=['stattrd']
+)
